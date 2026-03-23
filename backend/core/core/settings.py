8a76f439e3289e36c0a478764683aa3db67c59ed
Anup Bera <beraanup832@gmail.com>
2026-03-24 00:21:41 +0530

    Switch to SQLite for production

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'leadscout',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR / 'db.sqlite3',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
